890abbde7d8e1f38a5b8a5a2b2d6a133cc363d6c
gentlemanSoup <anders.horne@gmail.com>
2023-12-11 11:16:56 +0100

    Small updates

--- Runtime/Chains/ImxNFT.cs
+++ Runtime/Chains/ImxNFT.cs
@@ -1,21 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Pixygon.NFT
-{
-    public class ImxNFT : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+namespace Pixygon.NFT {
+    public class ImxNFT : MonoBehaviour {
     }
 }
